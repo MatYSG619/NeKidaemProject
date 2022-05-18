33e8f02f5f5b071767516b9faa9c09c4462b0821
Yaroslav <starstinger619@gmail.com>
2022-05-18 11:07:32 +0300

    create abstract user model

--- nekidaem/settings.py
+++ nekidaem/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from Blog_project import configure
+
+import configure
 
 from pathlib import Path
 
@@ -32,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user',
+    'accounts',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
